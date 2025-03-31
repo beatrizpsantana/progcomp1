9a48500e46053e391c9943ec5db23829d2458337
beatrizpsantana <beatrizdbz52@gmail.com>
2025-03-31 14:40:08 -0300

    main

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,10 +1,41 @@
-function calcular(){
-    //recupera o valor da ação social digitado
-    //declaração variável sem tipo
-     let acaoSocial = document.getElementById("acaoSocial").value
-     //recupera o valor da homenagem digitado
-     let Homenagem = document.getElementById("Homenagem").value
-     // vamos somar os valores
-     let soma = Number(acaoSocial) + Number(Homenagem)
-     alert(soma)
+function calcular() {
+    let soma = 0
+
+    //recupera o valor da Ação Social digitado
+    //declara uma variavel sem tipo
+    let acaoSocial = document.getElementById("acaoSocial").value
+    soma = soma + Number(acaoSocial)
+
+    let Homenagem = document.getElementById("Homenagem").value
+    soma = soma + Number(Homenagem)
+
+    let kitAlimentacao = document.getElementById("kitAlimentacao").value
+    soma = soma + (30 * Number(kitAlimentacao))
+
+    let lataSuplemento = document.getElementById("lataSuplemento").value
+    soma = soma + (15 * Number(lataSuplemento))
+
+    let pctArroz5 = document.getElementById("pctArroz5").value
+    soma = soma + (5 * Number(pctArroz5))
+
+    let pctArroz1 = document.getElementById("pctArroz1").value
+    soma = soma + Number(pctArroz1)
+
+    let feijao2 = document.getElementById("feijao2").value
+    soma = soma + (2 * Number(feijao2))
+
+    let feijao1 = document.getElementById("feijao1").value
+    soma = soma + Number(feijao1)
+
+    let macarrao = document.getElementById("macarrao").value
+    soma = soma + (0.5 * Number(macarrao))
+
+    let oleo = document.getElementById("oleo").value
+    soma = soma + Number(oleo)
+
+    let Leite = document.getElementById("Leite").value
+    soma = soma + (2 * Number(Leite))
+
+    //devolve o resultado para o html
+    document.getElementById("soma").innerHTML = soma.toFixed(2)
 }
