92084556d7e27418ccf7d8481f4a266b8d07d1e0
beatrizpsantana <beatrizdbz52@gmail.com>
2025-05-07 09:19:51 -0300

    pesquisaSatisfacao()

--- estrutura-repeticao/js/scripts.js
+++ estrutura-repeticao/js/scripts.js
@@ -14,3 +14,25 @@ function exe0(){
     //mmostra o resultado
     alert(`A média das notas é ${media.toFixed(1)}`)
 }
+
+function pesquisaSatisfacao(){
+    let nota
+    let participante = 1
+    let soma = 0
+    let satisfeitos = 0
+    let insatisfeito = 0
+    while(participante <= 10){
+        nota = Number(prompt(`Informe sua nota de satisfação (de 1 a 10) ${participante}`))
+        soma = soma + nota
+        participante++
+        if(nota >= 8){
+            satisfeitos++
+        }
+        else if(nota < 5){
+            insatisfeito++
+        }
+    }
+    let media = soma / 10
+    alert(`A média das notas é ${media.toFixed(1)}\nTotal de Satisfeitos: ${satisfeitos}\nTotal de Insatisfeitos: ${insatisfeito}`)
+
+} 
