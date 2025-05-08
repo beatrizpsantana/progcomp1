513984adf573921c395b308f711c349cacf63985
beatrizpsantana <beatrizdbz52@gmail.com>
2025-05-07 22:06:32 -0300

    continue

--- estrutura-repeticao/js/scripts.js
+++ estrutura-repeticao/js/scripts.js
@@ -16,21 +16,25 @@ function exe0(){
 }
 
 function pesquisaSatisfacao(){
-    let nota
-    let participante = 1
+    let nota //será informado pelo usuário
+    let participante = 1 //varia de 1 a 10
     let soma = 0
-    let satisfeitos = 0
-    let insatisfeito = 0
+    let satisfeitos = 0 //inicialmete, nenhum satisfeito
+    let insatisfeito = 0 //inicialmente, nenhum insatisfeito
     while(participante <= 10){
-        nota = Number(prompt(`Informe sua nota de satisfação (de 1 a 10) ${participante}`))
-        soma = soma + nota
+        nota = Number(prompt(`Informe sua nota de satisfação (de 0 a 10) ${participante}`))
+        if(nota >= 8 && nota<=10){
+            satisfeitos++ //satisfeitos = satisfeitos + 1
+        }
+        else if(nota>= 0 && nota < 5){ 
+            insatisfeito++ //insatisfeito = insatisfeito + 1
+        }
+        else if(nota<0 || nota>10){ // || é OU
+            alert(`Nota Inválida`)
+            continue //volta para o inicio sem incrementar participante 
+        }
         participante++
-        if(nota >= 8){
-            satisfeitos++
-        }
-        else if(nota < 5){
-            insatisfeito++
-        }
+        soma = soma + nota //soma += nota
     }
     let media = soma / 10
     alert(`A média das notas é ${media.toFixed(1)}\nTotal de Satisfeitos: ${satisfeitos}\nTotal de Insatisfeitos: ${insatisfeito}`)
